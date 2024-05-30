a6dbd2296664144b1b1df899ba1e1e2ee71998cd
Dean <111811264+DeanLemans@users.noreply.github.com>
2024-05-30 12:12:43 +0200

    obst spawn. changed player collision
    still need to add timer

--- scripts/player.gd
+++ scripts/player.gd
@@ -3,12 +3,6 @@ extends CharacterBody2D
 const GRAVITY : int = 4200
 const JUMP_POWER : int = -1800
 
-#func _physics_process(delta):
-	#if not is_on_floor():
-		#velocity.y += GRAVITY * delta
-	#if Input.is_action_just_pressed("jump") and is_on_floor():
-		#velocity.y = JUMP_POWER
-		
 func _physics_process(delta):
 	velocity.y += GRAVITY * delta
 	if is_on_floor() and Input.is_action_pressed("jump"):
